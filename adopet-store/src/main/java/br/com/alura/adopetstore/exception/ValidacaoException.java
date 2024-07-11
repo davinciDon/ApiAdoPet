e789b59548f2393f750c5cebd5fc0a4d725df5d4
onasc <o.nascimento.davi@gmail.com>
2024-07-11 18:56:03 -0300

    2

--- adopet-store/src/main/java/br/com/alura/adopetstore/exception/ValidacaoException.java
+++ adopet-store/src/main/java/br/com/alura/adopetstore/exception/ValidacaoException.java
@@ -2,6 +2,9 @@ package br.com.alura.adopetstore.exception;
 
 public class ValidacaoException extends RuntimeException {
     public ValidacaoException(String message) {
+      //  StringIndexOutOfBoundsException
         super(message);
     }
+
+
 }
